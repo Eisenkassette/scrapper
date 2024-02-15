5ce60ec32cfc8764d33cc3727330e8018c513991
Eisenkassette <daniel.larionov@protonmail.com>
2024-02-15 07:33:51 +0100

    Added more comments

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import csv
 
 # Finds all product URLs and formats them
 def find_append_urls():
+
     # To prevent duplication a temp variable is created
     temp_url_list = []
+
     # Scraps all URLs that lead to products
     for titre in soup.find_all("a", {"href": True, "title": True}):
         temp_url_list.append((titre['href']).encode('latin1').decode('UTF-8'))
@@ -18,18 +20,24 @@ def find_append_urls():
         full_url_list.append(item.replace('../../..', 'https://books.toscrape.com/catalogue'))
 
 
+# Appending data to the different lists
 def append_data():
-    # ------------------------------------- Appending data to the different lists -------------------------------------
+
+    # Appending the full url of the product
     product_page_url.append(full_url_list[a])
 
-    # The UPC code is always in a <td> which has a sibling <th> containing UPC
-    check = soup.find("th", string="UPC").find_next("td").text
+    # Appending the UPC code which is always in a <td> which has a sibling <th> containing the UPC
+    check = soup.find("th", string="UPC")
     if check is not None:
-        universal_product_code.append(check)
+        check = soup.find("th", string="UPC").find_next("td").text
+        if check is not None:
+            universal_product_code.append(check)
+        else:
+            universal_product_code.append("UPC Not Found")
     else:
         universal_product_code.append("UPC Not Found")
 
-    # Encoding and decoding is done to fix a problem with special characters
+    # Appending the title, encoding and decoding is done to fix a problem with special characters
     check = soup.find("h1").text.encode('latin1').decode('UTF-8')
     if check is not None:
         title.append(check)
@@ -64,18 +72,20 @@ def append_data():
                      .replace('../../', 'https://books.toscrape.com'))
 
 
+# Setting the main page of "books to scrape"
 main_url = "https://books.toscrape.com/index.html"
-product_url_list = []
+# Creating the empty list for storing the full urls of products
 full_url_list = []
+# Creating the empty list for storing all full urls of the different categories of the site
 full_category_url = []
 
-category_name = []
-
+# Creating a list for the headers used during the creation of csv files
 header = ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax",
           "number_available", "product_description", "category", "review_rating", "image_url"]
 
-# Creating dictionary to translate writen number ratings into numerical one in the loop
+# Creating dictionary to translate writen number ratings into numerical one
 word_to_number = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
+
 # All the required data to scrap
 product_page_url = []
 universal_product_code = []
@@ -88,6 +98,7 @@ category = []
 review_rating = []
 image_url = []
 
+# Creating the "output" folder
 os.makedirs("output", exist_ok=True)
 
 # Get the HTML from the main_url site
@@ -98,32 +109,52 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 # Find all the categories and appending the href content in a list
 categories = soup.find("ul", class_="nav nav-list").find_next("ul").find_all("a")
+
 href_list = [link['href'] for link in categories]
 
-# Transforming the relative URL into full paths
+# Transforming all the relative urls obtained above into full paths
 i = 0
 while i < len(href_list):
     full_category_url.append("https://books.toscrape.com/" + href_list[i])
     i += 1
 
+# Scrapping loop
 i = 0
 while i < len(full_category_url):
-    print("Scanning category ", i + 1, " out of ", len(full_category_url))
+
+    # Taking the active category url to later import for scanning
     url = full_category_url[i]
+
     response = requests.get(url)
+
     soup = BeautifulSoup(response.text, "html.parser")
-    # Checks if a "next" button is present on the page if so scraps the page and adds the next page and runs again,
-    # if no "next" button is present scraps the page and moves on.
+
+    # Note the current category to display it in the progress indicator and the csv name
+    category_title = soup.find("div", class_="page-header action").find_next("h1").get_text()
+
+    # Progress indicator
+    print("Scanning category ", i + 1, " out of ", len(full_category_url), " - ", category_title)
+
+    # Checks if a "next" button is present on the page if so scraps current page for product urls and
+    # adds the next page and runs again,
+    # if no "next" button is present scraps the current page and moves on.
     while (soup.find("a", string="next")) is not None:
         find_append_urls()
+
         next_page = soup.find("a", string="next").get('href')
+
+        # Removes the text after the last "/" in the link to replace it with "page x.html"
         url_last_slash = full_category_url[i].rfind("/")
         next_page = (full_category_url[i][:url_last_slash + 1] + next_page)
+
         response = requests.get(next_page)
+
         soup = BeautifulSoup(response.text, "html.parser")
     else:
         find_append_urls()
 
+    # Uses the previously obtained list of product's urls to scrap their information and append the information into
+    # the different lists
     a = 0
     while a != len(full_url_list):
         # Get the HTML for the page to scrap in this iteration from the full_url_list
@@ -136,13 +167,9 @@ while i < len(full_category_url):
         append_data()
 
         # Progress indicator
-        print("Scrapped : ", a + 1, " / ", len(full_url_list), " pages")
+        print("Scrapped : ", a + 1, " / ", len(full_url_list), " pages - ", title[a])
 
         a += 1
-
-    response = requests.get(full_category_url[i])
-    soup = BeautifulSoup(response.text, "html.parser")
-    category_title = soup.find("div", class_="page-header action").find_next("h1").get_text()
 
     with open("output/" + category_title + ".csv", mode="w", newline="") as file:
         writer = csv.writer(file)
